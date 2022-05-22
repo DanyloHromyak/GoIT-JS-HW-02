bc3526f185bc635c298161b0cb60e0097c09f92a
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-05-22 14:44:33 +0300

    first commit

---
+++ src/scripts/practical-task-01.js
@@ -0,0 +1,30 @@
+// Завдання 1
+const Arr1 = [1, 5, "4", "hello"];
+const Arr2 = [true, 2, {}, ["a"], 222];
+const sumArr = Arr1[1] + Arr2[1];
+console.log(sumArr);
+
+//Завдання 2
+Arr1.push(22);
+console.log(Arr1);
+
+// Завдання 3
+for (const item of Arr2) {
+  console.log(typeof item, item);
+}
+
+// Завдання 4
+const message = "Welcome to Ukraine!";
+const messageLetters = message.split("");
+console.log(messageLetters, message.indexOf("l"));
+
+const message2 = messageLetters.join("");
+console.log(message2);
+
+// Завдання 5
+const styles = ["Джаз", "Блюз"];
+styles.push("Рок-н-ролл");
+styles.splice(1, 1, "Класика");
+styles.shift();
+styles.unshift("Реп", "Реггі");
+console.log(styles);

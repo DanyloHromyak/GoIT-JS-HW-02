717385ec239e638348c78897ed1a0b8d7758f62d
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-05-28 13:19:15 +0300

    task 1 fixed

--- src/scripts/practical-task-01.js
+++ src/scripts/practical-task-01.js
@@ -2,10 +2,10 @@
 const Arr1 = [1, 5, "4", "hello"];
 const Arr2 = [true, 2, {}, ["a"], 222];
 const sumArr = Arr1[1] + Arr2[1];
-console.log(sumArr);
+console.log(`Сума: ${sumArr}, Доданки: ${Arr1[1]}, ${Arr2[1]}`);
 
 //Завдання 2
-Arr1.push(22);
+Arr1[Arr1.length] = 22;
 console.log(Arr1);
 
 // Завдання 3
@@ -17,7 +17,6 @@ for (const item of Arr2) {
 const message = "Welcome to Ukraine!";
 const messageLetters = message.split("");
 console.log(messageLetters, message.indexOf("l"));
-
 const message2 = messageLetters.join("");
 console.log(message2);
 
@@ -25,6 +24,6 @@ console.log(message2);
 const styles = ["Джаз", "Блюз"];
 styles.push("Рок-н-ролл");
 styles.splice(1, 1, "Класика");
-styles.shift();
+console.log(styles.shift());
 styles.unshift("Реп", "Реггі");
 console.log(styles);
